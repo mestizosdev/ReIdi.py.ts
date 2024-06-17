eae0c9014137f4931adf6fc4e5ef10aba2bf6c61
Jorge Luis <jorjoluiso@gmail.com>
2024-06-16 20:29:37 -0500

    [IMP] Database connection

--- ReIdi.ts/src/database/db.setup.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-// Connect to the MongoDB database
-const mongoDBURI = 'mongodb://localhost:27017';
-
-mongoose.connect(mongoDBURI);
-export default mongoose;
--- ReIdi.ts/src/index.ts
+++ ReIdi.ts/src/index.ts
@@ -1,10 +1,38 @@
 import { Hono } from 'hono'
+import { poweredBy } from 'hono/powered-by'
+import { logger } from 'hono/logger'
+
 import ping from './ping/ping'
 import version from './version/version'
+import dbConnect from './db/connect'
+import personModel from './db/persons.model'
 
 const app = new Hono()
 
+app.use(poweredBy())
+app.use(logger())
+
 app.route('/', ping)
-app.route('/', version)
+// app.route('/', version)
+
+dbConnect()
+  .then(() => {
+    app.get('/', async (c) => {
+      const persons = await personModel.find()
+      return c.json(
+        persons.map((p) => p.toObject()),
+        200
+      )
+    })
+  })
+  .catch((err) => {
+    app.get('/*', (c) => {
+      return c.text(`Failed to connect to database: ${err.message}`)
+    })
+  })
+
+app.onError((err, c) => {
+  return c.text(`App Error: ${err.message}`)
+})
 
 export default app
