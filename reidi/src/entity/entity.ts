97e7d990bbf46ae2e8ea1e073ce811df04b444cf
Jorge Luis <jorjoluiso@gmail.com>
2024-06-17 23:23:18 -0500

    [IMP] Identification validation

--- reidi/src/entity/entity.ts
+++ reidi/src/entity/entity.ts
@@ -1,6 +1,5 @@
 import { Hono } from 'hono'
 import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
 
 import personModel from '../db/person.model'
 import taxpayerModel from '../db/taxpayer.model'
@@ -16,39 +15,34 @@ const stringLenght10or13 = z
     }
   )
 
-entity.get(
-  '/entity/:identification',
-  zValidator('param', stringLenght10or13),
-  async (c) => {
-    const identification = c.req.param('identification')
-    /*
-  const m = stringLenght10or13.safeParse(identification)
-  console.log('m', m)
+entity.get('/entity/:identification', async (c) => {
+  const identification = c.req.param('identification')
 
-  if (!m.success) {
-    console.log('Validation failed:', m.error.errors)
+  const result = stringLenght10or13.safeParse(identification)
+
+  if (!result.success) {
+    return c.json(result.error.errors, 404)
   }
-*/
-    if (identification.length === 10) {
-      const person = await personModel.findOne({ identification })
 
-      if (!person) {
-        return c.json('Not found', 404)
-      }
+  if (identification.length === 10) {
+    const person = await personModel.findOne({ identification })
 
-      return c.json(person.toObject())
-    } else if (identification.length === 13) {
-      const taxpayer = await taxpayerModel.findOne({ identification })
-
-      if (!taxpayer) {
-        return c.json('Not found', 404)
-      }
-
-      return c.json(taxpayer.toObject())
+    if (!person) {
+      return c.json('Not found', 404)
     }
 
-    return c.json('Not found', 404)
+    return c.json(person.toObject())
+  } else if (identification.length === 13) {
+    const taxpayer = await taxpayerModel.findOne({ identification })
+
+    if (!taxpayer) {
+      return c.json('Not found', 404)
+    }
+
+    return c.json(taxpayer.toObject())
   }
-)
+
+  return c.json('Not found', 404)
+})
 
 export default entity
