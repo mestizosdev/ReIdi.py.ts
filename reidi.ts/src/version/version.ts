9cca6d66c57147a83292e91f76be0cf7d5e1dacf
Jorge Luis <jorjoluiso@gmail.com>
2024-08-22 12:14:35 -0500

    [REF] Upgrade code

--- reidi.ts/src/version/version.ts
+++ reidi.ts/src/version/version.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-import os from 'os'
+import { platform, release, arch } from 'os'
 import pkg from '../../package.json'
 import { version as serverVersion } from '../db/connect'
 
@@ -11,9 +11,9 @@ version.get('/version', async (c) => {
       name: 'ReIdi',
       author: pkg.author,
       version: pkg.version,
-      versionOS: os.platform() + ' ' + os.release() + ' ' + os.arch(),
+      versionOS: platform() + ' ' + release() + ' ' + arch(),
       versionRuntime: `Bun ${Bun.version}`,
-      versionDatabase: 'MongoDB ' + serverVersion
+      versionNoSqlDatabase: 'MongoDB ' + serverVersion
     },
     200
   )
